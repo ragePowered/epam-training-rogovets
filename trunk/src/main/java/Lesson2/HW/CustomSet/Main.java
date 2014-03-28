9e1e69ff0b2b2d0a458b6e95bba7cc3f3c34818d
andrew.rogovets@gmail.com <andrew.rogovets@gmail.com@45882356-1896-41b4-d121-8f43107bde62>
2014-03-28 22:16:15 +0000

    CustomSet pre-compile commit

---
+++ trunk/src/main/java/Lesson2/HW/CustomSet/Main.java
@@ -0,0 +1,32 @@
+package Lesson2.HW.CustomSet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrew
+ * Date: 3/28/14
+ * Time: 11:58 PM
+ */
+
+interface Container{
+    public void put();
+    public void get(int index);
+    public void remove(int index);
+    public int size();
+}
+
+interface Element{
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class CustomSet{
+    private Container container;
+
+    public void performAdd(Element element){
+
+    }
+}
