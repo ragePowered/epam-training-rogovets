504dc81cf729b2dd20c35d6d517c6d9ce4dc4a75
andrew.rogovets@gmail.com <andrew.rogovets@gmail.com@45882356-1896-41b4-d121-8f43107bde62>
2014-03-28 23:29:38 +0000

    CustomSet with CustomArrayList<T>

--- src/main/java/Lesson2/HW/CustomSet/Main.java
+++ src/main/java/Lesson2/HW/CustomSet/Main.java
@@ -1,5 +1,7 @@
 package Lesson2.HW.CustomSet;
 
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: andrew
@@ -7,26 +9,124 @@ package Lesson2.HW.CustomSet;
  * Time: 11:58 PM
  */
 
-interface Container{
-    public void put();
-    public void get(int index);
-    public void remove(int index);
-    public int size();
-}
-
-interface Element{
+interface Container<T>{
+    public void performPut(T element);
+    public T performGet(int index);
+    public void performRemove(int index);
+    public boolean performContains(T element);
+    public int getSize();
 }
 
 public class Main {
     public static void main(String[] args) {
+		CustomSet<Integer> set = new CustomSet<Integer>(new CustomArrayList<Integer>());
+		for (int i = 0; i < 5; i++){
+			set.add((int)(Math.random() * 11));
+		}
+		set.showAllElements();
 
-    }
+		set.remove(0);
+		set.remove(set.size() - 1);
+
+		set.showAllElements();
+
+		set.add(set.get(set.size() - 1));
+
+		set.showAllElements();
+
+		System.out.println("Contains 5 ? - " + set.contains(5));
+	}
 }
 
-class CustomSet{
-    private Container container;
+class CustomSet<T>{
+    private Container<T> container;
 
-    public void performAdd(Element element){
-
+    CustomSet(Container<T> container) {
+        this.container = container;
     }
+
+    public void add(T element){
+        this.container.performPut(element);
+    }
+
+    public T get(int index){
+		return this.container.performGet(index);
+    }
+
+	public void remove(int index){
+		this.container.performRemove(index);
+	}
+
+	public boolean contains(T element){
+		return this.container.performContains(element);
+	}
+
+	public int size(){
+		return this.container.getSize();
+	}
+
+	public void showAllElements(){
+		for (int i = 0; i < container.getSize(); i++){
+			System.out.print(container.performGet(i) + " ");
+		}
+		System.out.println();
+	}
 }
+
+class CustomArrayList<T> extends ArrayList<T> implements Container<T>{
+	private ArrayList<T> list = new ArrayList<T>();
+
+	@Override
+	public void performPut(T element) {
+		list.add(element);
+	}
+
+	@Override
+	public T performGet(int index) {
+		return list.get(index);
+	}
+
+	@Override
+	public void performRemove(int index) {
+		list.remove(index);
+	}
+
+	@Override
+	public boolean performContains(T element) {
+		return list.contains(element);
+	}
+
+	@Override
+	public int getSize() {
+		return list.size();
+	}
+}
+
+/*
+class CustomLinkedList<T> extends LinkedList<T> implements Container<T>{
+
+	@Override
+	public void performPut(T element) {
+		//Implemented
+	}
+
+	@Override
+	public T performGet(int index) {
+		return null;  //Implemented
+	}
+
+	@Override
+	public void performRemove(int index) {
+		//Implemented
+	}
+
+	@Override
+	public boolean performContains(T element) {
+		return false;  //Implemented
+	}
+
+	@Override
+	public int getSize() {
+		return 0;  //Implemented
+	}
+}*/
