6bae996a57220291d7a3f7fbbfa1dbc0b8f944f2
andrew.rogovets@gmail.com <andrew.rogovets@gmail.com@45882356-1896-41b4-d121-8f43107bde62>
2014-04-10 23:18:00 +0000

    Figures 1.0

---
+++ src/main/java/Lesson6/HW/Main.java
@@ -0,0 +1,14 @@
+package Lesson6.HW;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andrew
+ * Date: 4/9/14
+ * Time: 9:02 PM
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(Plane.getInstance().getAsList());
+	}
+}
