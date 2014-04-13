e2b76f97536bf490a317a6b20c67f7c87d824f48
andrew.rogovets@gmail.com <andrew.rogovets@gmail.com@45882356-1896-41b4-d121-8f43107bde62>
2014-04-13 22:30:07 +0000

    Violinists, Deadlock 2.0 Program rewritten!

--- trunk/src/main/java/Lesson7/HW/Violins/Main.java
+++ trunk/src/main/java/Lesson7/HW/Violins/Main.java
@@ -7,51 +7,100 @@ package Lesson7.HW.Violins;
  * Time: 6:42 PM
  */
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Есть 2 скрипача, скрипока и смычок. Смоделировать Deadlock и разрешить его
  * стандартными средствами Java 1.4
  */
 
 public class Main {
-	public static final Violin violin = new Violin();
-	public static final Bow bow = new Bow();
+	private static volatile Queue<Violin> violins = new LinkedList<>();
+	private static volatile Queue<Bow> bows = new LinkedList<>();
 
-	public static void main(String[] args) {
-		new Violinist().start();
-		new LeftHandViolinist().start();
-	}
-
-	private static class Violinist extends Thread{
-		@Override
-		public void run() {
-			synchronized (violin){
-				System.out.println("Vilinist holding " + violin);
-				sleepTime();
-				System.out.println("Vilinist waiting to take " + bow);
-				synchronized (bow){
-					System.out.println("Vilinist playing Beethoven");
-				}
-			}
+	static {
+		for (int i = 0; i < 3; i++){
+			violins.add(new Violin(i+1));
+		}
+		for (int i = 0; i < 2; i++){
+			bows.add(new Bow(i+1));
 		}
 	}
 
-	private static class LeftHandViolinist extends Thread{
+	public static void main(String[] args) {
+		new Violinist("Andrew").start();
+		new Violinist("Boriss").start();
+		new Violinist("Sergii").start();
+		new Violinist("Alexey").start();
+	}
+
+	private static class Violinist extends Thread{
+
+		Violinist(String name){
+			setName(name);
+		}
+
 		@Override
 		public void run() {
-			synchronized (violin){
-				System.out.println("Left handed vilinist holding " + violin);
-				sleepTime();
-				System.out.println("Left handed vilinist waiting to take " + bow);
-				synchronized (bow){
-					System.out.println("Left handed vilinist playing Beethoven");
+			Violin violin;
+			Bow bow;
+			int i = 0;
+			while (i < 3){
+				synchronized (violins){
+					if ((violin = violins.poll()) != null){
+						System.out.println(getName() + " got " + violin);
+					} else {
+						System.out.println(getName() + " waiting for VIOLIN");
+						try {
+							currentThread().join(600);
+						} catch (InterruptedException e) {
+							e.printStackTrace();  //Exception here
+						}
+					}
 				}
+
+				sleepTime();
+
+				synchronized (bows){
+					if ((bow = bows.poll()) != null){
+
+						System.out.println(getName() + " got " + bow);
+
+						if (violin != null){
+							System.out.println(getName() + " playing Beethoven on " + violin + " and " + bow);
+						}
+
+					} else {
+
+						System.out.println(getName() + " waiting for BOW");
+						try {
+							currentThread().join(600);
+						} catch (InterruptedException e) {
+							e.printStackTrace();  //Exception here
+						}
+					}
+				}
+
+				if (bow != null) {
+					bows.offer(bow);
+					bow = null;
+				}
+
+				if (violin != null) {
+					violins.offer(violin);
+					violin = null;
+				}
+
+				i++;
 			}
+
 		}
 	}
 
 	private static void sleepTime(){
 		try {
-			Thread.sleep(1000);
+			Thread.sleep((int)(Math.random() * 751 + 250));
 		} catch (InterruptedException e) {
 			e.printStackTrace();  //Exception here
 		}
@@ -59,15 +108,35 @@ public class Main {
 }
 
 class Violin{
+	private Integer id;
+
+	Violin(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
 	@Override
 	public String toString() {
-		return "violin";
+		return "Violin #" + id;
 	}
 }
 
 class Bow{
+	private Integer id;
+
+	Bow(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
 	@Override
 	public String toString() {
-		return "bow";
+		return "Bow #" + id;
 	}
 }
